34ab825c6ab0af1e74377307c5ebe9fa63020af2
Sirvach Ansari <sirvachansaribusiness@gmail.com>
2024-03-15 13:08:17 -0400

    LAB 8

--- sort.c
+++ sort.c
@@ -5,9 +5,9 @@
 
 int extraMemoryAllocated;
 
-void *Alloc(size_t sz)
+void *Alloc(size_t sz) 
 {
-	extraMemoryAllocated += sz;
+	extraMemoryAllocated += sz; 
 	size_t* ret = malloc(sizeof(size_t) + sz);
 	*ret = sz;
 	printf("Extra memory allocated, size: %ld\n", sz);
@@ -29,8 +29,69 @@ size_t Size(void* ptr)
 
 // implement merge sort
 // extraMemoryAllocated counts bytes of extra memory allocated
+void merge(int pData[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+ 
+	// Allocate memory
+	int *L = (int *)Alloc(sizeof(int) * n1);
+	int *R = (int *)Alloc(sizeof(int) * n2);
+ 
+	// Copy data to temp arrays L[] and R[]
+	for (i = 0; i < n1; i++)
+		L[i] = pData[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = pData[m + 1 + j];
+ 
+	// Merge the temp arrays back into pData[l..r]
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			pData[k] = L[i];
+			i++;
+		}
+		else {
+			pData[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+ 
+	// Copy the remaining elements of L[], if there are any
+	while (i < n1) {
+		pData[k] = L[i];
+		i++;
+		k++;
+	}
+ 
+	// Copy the remaining elements of R[], if there are any
+	while (j < n2) {
+		pData[k] = R[j];
+		j++;
+		k++;
+	}
+	DeAlloc(L);
+	DeAlloc(R);
+}
+	// implement merge sort
+	// extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r) {
+		// Same as (l+r)/2, but avoids overflow for large l and h
+		int m = l + (r - l) / 2;
+ 
+		// Sort first and second halves
+		mergeSort(pData, l, m);
+		mergeSort(pData, m + 1, r);
+ 
+		merge(pData, l, m, r);
+	}
+
 }
 
 // parses input file to an integer array
@@ -67,19 +128,22 @@ int parseData(char *inputFileName, int **ppData)
 // prints first and last 100 items in the data array
 void printArray(int pData[], int dataSz)
 {
-	int i, sz = dataSz - 100;
-	printf("\tData:\n\t");
-	for (i=0;i<100;++i)
-	{
-		printf("%d ",pData[i]);
-	}
-	printf("\n\t");
+	int i;
 	
-	for (i=sz;i<dataSz;++i)
+	if (dataSz > 200)
 	{
-		printf("%d ",pData[i]);
+		for (i=0;i<100;++i)
+			printf("%d ",pData[i]);
+		printf("... ");
+		for (i=dataSz-100;i<dataSz;++i)
+			printf("%d ",pData[i]);
 	}
-	printf("\n\n");
+	else
+	{
+		for (i=0;i<dataSz;++i)
+			printf("%d ",pData[i]);
+	}
+	printf("\n");
 }
 
 int main(void)
